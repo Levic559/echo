dd0d84bc66496b7d60ad32e4c602b467b6d70595
LeviChen559 <j89311025@gmail.com>
2022-02-11 15:15:30 -0800

    nav

--- comps/Card.js
+++ comps/Card.js
@@ -3,19 +3,19 @@ import MyButton from "./Button"
 
 export const Card={
     one:{
-        component1:<InputBox text='Account'/>,
-        component2:<InputBox text='Password'/>,
+        component1:<InputBox text='Account :'/>,
+        component2:<InputBox text='Password :'/>,
         
 
     }, 
    two:{
-        component1:<InputBox text='User Name'/>,
-        component2:<InputBox text='Gender'/>,
+        component1:<InputBox text='User Name :'/>,
+        component2:<InputBox text='Gender :'/>,
        
     },
     third:{
-        component1:<InputBox text='Age'/>,
-        component2:<InputBox text='Location'/>,
+        component1:<InputBox text='Age :'/>,
+        component2:<InputBox text='Location :'/>,
        
     }
 }
--- comps/Nav.js
+++ comps/Nav.js
@@ -1,25 +1,33 @@
 import React from "react";
-import styled from "styled-components";
 import Icon from "./Icon";
-import {header_theme, text_theme} from'../utils/variables'
-import {useTheme} from '../utils/provider'
+import { header_theme, text_theme } from '../utils/variables'
+import { useTheme } from '../utils/provider'
 import { Router } from "next/router";
 import { useRouter } from "next/router";
+import { Input } from 'semantic-ui-react'
 
 
+const Nav = ({
+    text = 'Setting',
+}) => {
 
-const Nav =({
-    text='Setting',
-})=>{
-
-    const {theme} = useTheme();
-    const router=useRouter()
-    return(
+    const { theme } = useTheme();
+    const router = useRouter()
+    return (
         <div className="navBar" BGC={header_theme[theme].label}>
-          {/* <Icon/> */}
-          <div color={text_theme[theme].label} onClick={()=>router.push('/books')}>{text}</div>
-          {/* <Icon HomeDis="none" GearDis="block"/> */}
-          </div>
+            <div className="navLogoCon">
+                Echo
+            </div>
+
+            <div className="navInputCon">
+
+                <Input />
+                <Input />
+            </div>
+
+            <div color={text_theme[theme].label} onClick={() => router.push('/books')}>{text}</div>
+
+        </div>
     )
 }
 
