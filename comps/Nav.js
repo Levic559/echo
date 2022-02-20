a4d2ceb1349a39985a0a253f7bc25c34b5b42415
Levic559 <ychen559@my.bcit.ca>
2022-02-19 22:16:17 -0800

    nav

--- comps/Nav.js
+++ comps/Nav.js
@@ -7,14 +7,21 @@ import { Input } from 'semantic-ui-react'
 import Logo from "./Logo";
 import NavLogo from "./NavLogo";
 import { Icon } from 'semantic-ui-react'
-
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
 const Nav = ({
     users = 'Guest',
 }) => {
 
     const { theme } = useTheme();
     const router = useRouter()
+    const top100Films = [
+        { label: 'Author' },
+        { label: 'ISBN' },
+        
+      ];
     return (
+        
         <div className="navBar" BGC={header_theme[theme].label}>
             <div className="navLogoCon  " onClick={()=>router.push("/bookShelf")}>
                 <NavLogo  className="lightColor " />
@@ -23,8 +30,15 @@ const Nav = ({
 
             <div className="navInputCon">
 
-            <Input   className="navInput"  placeholder='Search...' />
-            <Input className="navInput" placeholder='Search...' />
+            <input   className="input"  placeholder='Search...'  />
+            
+            <Autocomplete
+      disablePortal
+      id="combo-box-demo"
+      options={top100Films}
+      sx={{ width: 100, background:"#cad2c5", borderRadius:1.5 }}
+      renderInput={(params) => <TextField {...params} label="filiter" />}
+    />
             </div>
             <div className="navUsers ">
                Hello~ {users}
