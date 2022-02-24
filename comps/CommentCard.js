6abfdfbcc2afad7a4735d4de67a84da8ffb2efc5
Levic559 <ychen559@my.bcit.ca>
2022-02-23 19:57:10 -0800

    fixed text color on the comment card

--- comps/CommentCard.js
+++ comps/CommentCard.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import Image from 'next/image'
+import {comp_theme, text_theme} from'../utils/variables'
+import {useTheme} from '../utils/provider'
 const default_date={
   "booksrc":"http://images.amazon.com/images/P/0002005018.01.LZZZZZZZ.jpg",
   "comment":"Every literate person is aware of the paragraph. Paragraphs are important in speeches, in writing or a random paragraph can be used as a prop in ads or presentations too! They are like the building blocks. Since paragraphs have so many roles to play and it is needed, websites on the internet have developed a tool called Random Paragraph Generator which is a great help for those who are in search of random paragraphs for working purposes.",
@@ -18,6 +20,7 @@ const CommentCard=({
 
 })=>{
 
+  const {theme} = useTheme();
 
 
     return <div className='CommentCard_Con'>
@@ -25,10 +28,16 @@ const CommentCard=({
 
         <Image src={booksrc} width={275}
       height={275} objectFit="cover"/>
-        <div className='content'> {comment}   </div>
+        <div className='content'style={{
+      
+      color:text_theme[theme].title
+      }}> {comment}   </div>
         <div className='user'> 
             <img src={usersrc}/>
-            <h3> {username}</h3>
+            <h3 style={{
+      
+      color:text_theme[theme].title
+      }}> {username}</h3>
             <Button variant="contained">more</Button>
           </div>
 
