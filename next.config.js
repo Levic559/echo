750c09139474956c556e73862ae06048fee5f79d
ChenWarren <72281162+ChenWarren@users.noreply.github.com>
2022-08-29 16:32:59 -0700

    update config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['images.amazon.com','news.artnet.com','images.unsplash.com', 'echoserver-env.eba-bccqr6dt.us-east-1.elasticbeanstalk.com', 'localhost']
+    domains: ['news.artnet.com','images.unsplash.com', 'pure-sierra-91252.herokuapp.com', 'localhost']
   },
   env: {
-    BASE_URL: process.env.BASE_URL,
+    BASE_URL: "https://pure-sierra-91252.herokuapp.com",
     LOCAL_URL:"http://localhost:3030",
-    SOCKET_URL:process.env.SOCKET_URL,
+    SOCKET_URL:"https://echo-clubchat.herokuapp.com",
   }
 }
