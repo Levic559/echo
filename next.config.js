e76b3b86564759f3a82859b3814bcaa33af5709d
ChenWarren <72281162+ChenWarren@users.noreply.github.com>
2022-08-29 16:46:58 -0700

    update config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['news.artnet.com','images.unsplash.com', 'pure-sierra-91252.herokuapp.com', 'localhost']
+    domains: ['images.amazon.com','news.artnet.com','images.unsplash.com', 'pure-sierra-91252.herokuapp.com', 'localhost']
   },
   env: {
     BASE_URL: "https://pure-sierra-91252.herokuapp.com",
